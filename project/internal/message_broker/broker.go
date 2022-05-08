dc59d9a23c08834619a0c018fbfb16c0b00d08ac
justdiasik <71446509+justdiasik@users.noreply.github.com>
2022-05-08 20:46:11 +0600

    Add files via upload

---
+++ project/internal/message_broker/broker.go
@@ -0,0 +1,19 @@
+package message_broker
+
+import "context"
+
+type (
+
+	MessageBroker interface {
+		Connect(ctx context.Context) error
+		Close() error
+
+		Cache() CacheBroker // Сюда отправляются команды, связанные непосредственно с кэшом
+	}
+
+	BrokerWithClient interface {
+		Connect(ctx context.Context, brokers []string) error
+		Close() error
+	}
+)
+
